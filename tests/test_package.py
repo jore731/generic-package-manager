7c9ca64ba2edeed3805cf2c0fb8bb68769779f94
Jorge Pulido <jorge.pulido@hotmail.com>
2021-09-11 20:29:11 +0200

    Importation naming fixed and few naming convention changes

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,7 +1,8 @@
 import logging
 import unittest
 
-import package
+from package import Package, PackageInstallationError, DependencyInstallationError, PackageRemovalError, \
+    DependenceLoopError
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(name)s [%(levelname)s]: %(message)s")
 
@@ -14,36 +15,33 @@ class PackageInstallingTest(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_importing(self):
-        self.assertIsNotNone(package)
-
     def test_create_package(self):
-        new_package = package.Package("nano")
+        new_package = Package("nano")
         self.assertEqual(new_package.name, "nano")
 
     def test_install_package(self):
-        new_package = package.Package("nano")
-        new_package.install(True)
+        new_package = Package("nano")
+        new_package.install(explicitly_installed=True)
         self.assertTrue(new_package.installed)
         self.assertTrue(new_package.explicitly_installed)
 
     def test_package_not_installed(self):
-        new_package = package.Package("nano")
+        new_package = Package("nano")
         self.assertFalse(new_package.installed)
 
     def test_register_dependency(self):
-        self.main_package = package.Package("main_package")
-        self.dependency = package.Package("dependency")
-        self.dependency2 = package.Package("dependency2")
-        self.main_package.depends_on(self.dependency)
+        self.main_package = Package("main_package")
+        self.dependency1 = Package("dependency1")
+        self.dependency2 = Package("dependency2")
+        self.main_package.depends_on(self.dependency1)
         self.main_package.depends_on(self.dependency2)
-        self.assertEqual(self.main_package.dependencies[0].name, "dependency")
+        self.assertEqual(self.main_package.dependencies[0].name, "dependency1")
 
     def test_install_package_with_dependencies(self):
         self.test_register_dependency()
-        self.main_package.install(True)
-        self.assertTrue(self.dependency.installed)
-        self.assertFalse(self.dependency.explicitly_installed)
+        self.main_package.install(explicitly_installed=True)
+        self.assertTrue(self.dependency1.installed)
+        self.assertFalse(self.dependency1.explicitly_installed)
         self.assertTrue(self.dependency2.installed)
         self.assertFalse(self.dependency2.explicitly_installed)
         self.assertTrue(self.main_package.installed)
@@ -52,10 +50,10 @@ class PackageInstallingTest(unittest.TestCase):
     def test_installation_failed(self):
         self.test_register_dependency()
         self.main_package.install_process = raise_exception
-        self.assertRaises(package.PackageInstallationError, self.main_package.install)
+        self.assertRaises(PackageInstallationError, self.main_package.install)
 
     def test_installation_failed_on_dependency(self):
         self.test_register_dependency()
-        self.dependency.install_process = raise_exception
-        self.assertRaises(package.DependencyInstallationError, self.main_package.install)
+        self.dependency1.install_process = raise_exception
+        self.assertRaises(DependencyInstallationError, self.main_package.install)
 
